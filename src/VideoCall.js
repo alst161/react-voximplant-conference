596c8233f2a9eea08b2f0b8ae91be55787f7614f
alst161 <alst161@gmail.com>
2019-10-10 01:10:35 +0300

    mail

--- src/VideoCall.js
+++ src/VideoCall.js
@@ -13,8 +13,8 @@ class VideoCall extends Component{
         this.voxAPI.on(VoxImplant.Events.ConnectionFailed, (e) => this.voxConnectionFailed(e));
         this.voxAPI.on(VoxImplant.Events.ConnectionClosed, (e) => this.voxConnectionClosed(e));
         this.voxAPI.on(VoxImplant.Events.AuthResult, (e) => this.voxAuthEvent(e));
-        /*this.voxAPI.on(VoxImplant.Events.MicAccessResult, (e) => this.voxMicAccessResult(e));
-        this.voxAPI.on(VoxImplant.Events.IncomingCall, (e) => this.voxIncomingCall(e));*/
+        this.voxAPI.on(VoxImplant.Events.IncomingCall, (e) => this.voxIncomingCall(e));
+        /*this.voxAPI.on(VoxImplant.Events.MicAccessResult, (e) => this.voxMicAccessResult(e));*/
 
 
     }
@@ -22,11 +22,8 @@ class VideoCall extends Component{
     init() {
         try {
             this.voxAPI.init({
-                micRequired: true,
-                videoSupport: true,
-                localVideoContainerId: "call_localvideo"
+                remoteVideoContainerId: "call_remote_video"
             });
-            this.voxAPI.showLocalVideo(true)
         } catch(e) {
             this.setState({
                 msg: "Необходим браузер с поддержкой технологии WebRTC, воспользуйтесь, пожалуйста, Chrome/Chromium, Firefox или Opera"
@@ -50,6 +47,22 @@ class VideoCall extends Component{
         console.log("VoxImplant connected");
         console.log(this.props.login)
         this.voxAPI.requestOneTimeLoginKey(this.props.login+"@conference.arizukiii.voximplant.com");
+    }
+
+    voxIncomingCall(e){
+        console.log(`[WebSDk] New incoming call with ID: ${e.call.id()}`);
+        if(this.call){
+            console.log('[WebSDk] You already have active call. Hangup;');
+            e.call.hangup();
+        }else{
+            this.call = e.call;
+            this.call.on(VoxImplant.CallEvents.Connected, this.onCallConnected);
+            this.call.on(VoxImplant.CallEvents.Disconnected, this.onCallDisconnected);
+            this.call.on(VoxImplant.CallEvents.Failed, this.onCallFailed);
+            this.call.on(VoxImplant.CallEvents.EndpointAdded, this.onEndpointAdded);
+            this.call.answer();
+        }
+
     }
 
     voxConnectionFailed(e) {
@@ -86,7 +99,7 @@ class VideoCall extends Component{
     }
 
     startCall(){
-        this.call = this.voxAPI.callConference({number: 100, video: {sendVideo: true, receiveVideo: true}})
+        this.call = this.voxAPI.callConference({number: 111, video: {sendVideo: true, receiveVideo: true}})
         this.call.on(VoxImplant.CallEvents.Connected, this.onCallConnected);
         this.call.on(VoxImplant.CallEvents.Disconnected, this.onCallDisconnected);
         this.call.on(VoxImplant.CallEvents.Failed, this.onCallFailed);
@@ -110,9 +123,22 @@ class VideoCall extends Component{
     onEndpointAdded(e) {
         console.log(`[WebSDk] New endpoint ID: ${e.endpoint.id} (${e.endpoint.isDefault?'default':'regular'}) for Call ID: ${e.call.id()}`);
         //Remove the display element with this endpoint
-        e.endpoint.on(VoxImplant.EndpointEvents.Removed, this.onEndpointRemoved);
-        e.endpoint.on(VoxImplant.EndpointEvents.RemoteMediaAdded, this.onRemoteMediaAdded);
-        e.endpoint.on(VoxImplant.EndpointEvents.RemoteMediaRemoved, this.onRemoteMediaRemoved);
+        e.endpoint.on(VoxImplant.EndpointEvents.RemoteMediaAdded, (event) => {
+            console.log(event)
+        });
+        e.endpoint.on(VoxImplant.EndpointEvents.InfoUpdated,(event) => this.onInfoUpdate(event));
+        e.endpoint.on(VoxImplant.EndpointEvents.Removed, (event) => {
+            console.log(`[WebSDk] New MediaRenderer ID: ${e.mediaRenderer.id} in ${e.endpoint.id} for Call ID: ${e.call.id()}`);
+            const endpointNode = document.getElementById("call_remote_video");
+            if(endpointNode){
+                console.log(endpointNode)
+                e.mediaRenderer.render(endpointNode);
+            }
+        });
+        e.endpoint.on(VoxImplant.EndpointEvents.RemoteMediaRemoved, (event) => this.onRemoteMediaRemoved(event));
+    }
+    onInfoUpdate(e){
+        console.log(e)
     }
     onEndpointRemoved(e) {
         console.log(`[WebSDk] Endpoint was removed ID: ${e.endpoint.id} (${e.endpoint.isDefault?'default':'regular'}) for Call ID: ${e.call.id()}`);
@@ -120,7 +146,7 @@ class VideoCall extends Component{
 
     onRemoteMediaAdded(e) {
         console.log(`[WebSDk] New MediaRenderer ID: ${e.mediaRenderer.id} in ${e.endpoint.id} for Call ID: ${e.call.id()}`);
-        const endpointNode = document.getElementById("call_remotevideo");
+        const endpointNode = document.getElementById("call_remote_video");
         if(endpointNode){
             e.mediaRenderer.render(endpointNode);
         }
@@ -138,7 +164,7 @@ class VideoCall extends Component{
         return(
                 <div>
                     <div id="call_localvideo" style={{width:"300px",height:"300px"}}/>
-                    <div id="call_remotevideo" style={{width:"300px",height:"300px"}}/>
+                    <div id="call_remote_video" style={{width:"300px",height:"300px"}}/>
                 </div>
             )
     }
